beaa1d11c4074668b9e8200a0ebfc6e2415b6550
tianhe1986 <w1s2j3229@163.com>
2020-04-30 16:05:22 +0800

    匹配处理。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- game/command.go
+++ game/command.go
@@ -1,5 +1,12 @@
 package game
 
+// 发起匹配content
 type MatchCommand struct {
 	Name int `json:"name"`
+}
+
+// 匹配结果content
+type MatchResultCommand struct {
+	Players []string `json:"players"`
+	RoomId int `json:"roomId"`
 }
--- socket/gameServer.go
+++ socket/gameServer.go
@@ -8,6 +8,12 @@ import (
 	"strconv"
 )
 
+type MatchItem struct {
+	ws *Client
+	name string
+	seq int
+}
+
 type GameServer struct {
 	// 匹配队列
 	queue *list.List
@@ -53,6 +59,57 @@ func (gameServer *GameServer) handleMsg(client *Client, message *Message) {
 // 进入匹配队列，尝试匹配
 func (gameServer *GameServer) matchPlayer(client *Client, name string, seq int) {
 	log.Printf("user %s try match", name)
+	newMatchItem := MatchItem{
+		ws: client,
+		name: name,
+		seq: seq,
+	}
+	
+	gameServer.queue.PushBack(newMatchItem)
+
+	// 每三个进行处理
+	if gameServer.queue.Len() == 3 {
+		var players []MatchItem = make([]MatchItem, 3)
+		var names []string = make([]string, 3)
+		var roomPlayers []RoomPlayItem = make([]RoomPlayItem, 3)
+
+		for i := 0; i < 3; i++ {
+			tempItem := gameServer.queue.Front()
+			gameServer.queue.Remove(tempItem)
+			p, ok := tempItem.Value.(MatchItem);
+			if ! ok {
+				return
+			}
+
+			players[i] = p
+			names[i] = p.name
+			playItem := RoomPlayItem{
+				ws: p.ws,
+				name: p.name,
+			}
+			roomPlayers[i] = playItem
+		}
+
+		// 发送匹配成功消息
+		resultContent := game.MatchResultCommand{}
+		resultContent.Players = names
+		resultContent.RoomId = gameServer.roomIndex
+		for i := 0; i < 3; i++ {
+			resp := Message{}
+			resp.Code = 0
+			resp.Command = MATCH_PLAYER
+			resp.Seq = players[i].seq
+			resp.Content, _ = json.Marshal(resultContent)
+
+			tempMsg, _ := json.Marshal(resp)
+			players[i].ws.send  <- tempMsg
+		}
+
+		room := RoomServer{}
+		room.roomId = gameServer.roomIndex;
+		gameServer.roomIndex++;
+		gameServer.rooms[room.roomId] = room
+	}
 }
 
 // 单局游戏内消息

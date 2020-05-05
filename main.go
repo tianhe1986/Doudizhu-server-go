87381561126afb7a8202f3f044f1676606d72e03
tianhe1986 <w1s2j3229@163.com>
2020-05-05 16:58:00 +0800

    清除无用的html处理。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- main.go
+++ main.go
@@ -10,24 +10,10 @@ import (
 
 var addr = flag.String("addr", ":8181", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	hub := socket.NewHub()
 	go hub.Run()
-	http.HandleFunc("/test", serveHome)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		socket.ServeWs(hub, w, r)
 	})
